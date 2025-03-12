2ffaa1e099b52d4435b5c903989cafe86271b5a2
Konstantin S <havendv@gmail.com>
2025-03-12 15:56:39 +0400

    test: Fixed.

---
+++ src/tests/CSharpToJsonSchema.SnapshotTests/Snapshots/ToolTests.WeatherStrict#IWeatherStrictTools.Tools.generated.verified.cs
@@ -0,0 +1,31 @@
+﻿//HintName: IWeatherStrictTools.Tools.generated.cs
+
+#nullable enable
+
+namespace CSharpToJsonSchema.IntegrationTests
+{
+    public static partial class WeatherStrictToolsExtensions
+    {
+        public static global::System.Collections.Generic.IList<global::CSharpToJsonSchema.Tool> AsTools(this IWeatherStrictTools functions)
+        {
+            return new global::System.Collections.Generic.List<global::CSharpToJsonSchema.Tool>
+            {
+                new global::CSharpToJsonSchema.Tool
+                {
+                    Name = "GetCurrentWeather2",
+                    Description = "Get the current weather in a given location",
+                    Strict = true,
+                    Parameters = global::CSharpToJsonSchema.SchemaBuilder.ConvertToSchema(global::CSharpToJsonSchema.IntegrationTests.WeatherStrictToolsExtensionsJsonSerializerContext.Default.GetCurrentWeather2Args,"{\"mainFunction_Desc\":\"Get the current weather in a given location\"}"),
+                },
+
+                new global::CSharpToJsonSchema.Tool
+                {
+                    Name = "GetCurrentWeatherAsync2",
+                    Description = "Get the current weather in a given location",
+                    Strict = true,
+                    Parameters = global::CSharpToJsonSchema.SchemaBuilder.ConvertToSchema(global::CSharpToJsonSchema.IntegrationTests.WeatherStrictToolsExtensionsJsonSerializerContext.Default.GetCurrentWeatherAsync2Args,"{\"mainFunction_Desc\":\"Get the current weather in a given location\"}"),
+                },
+            };
+        }
+    }
+}
